26b409963d8f2702b84822ee554f4479585a2862
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-01-15 13:31:29 +0100

    renable python 3.5

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         "rlp>=1.0.0,<2"
     ],
     setup_requires=['setuptools-markdown'],
-    python_requires='>=3.6, <4',
+    python_requires='>=3.5, <4',
     extras_require=extras_require,
     py_modules=['eth_account'],
     license="MIT",
