15f7335ef846244074bfc1ab49855a50c926e7c7
Bryant Eisenbach <fubuloubu@gmail.com>
2020-02-01 23:09:22 -0500

    test: Added test from ganache-core

--- tests/core/test_hdaccount.py
+++ tests/core/test_hdaccount.py
@@ -1,15 +1,33 @@
+import pytest
+
 from eth_account import (
     Account,
 )
 
 
-# https://github.com/MetaMask/eth-hd-keyring/blob/master/test/index.js
-def test_account_derivation():
-    words = "finish oppose decorate face calm tragic certain desk hour urge dinosaur mango"
-    a1 = Account.from_mnemonic(words)
-    a2 = Account.from_mnemonic(words, account_index=1)
-    assert a1.address == "0x1c96099350f13D558464eC79B9bE4445AA0eF579"
-    assert a2.address == "0x1b00AeD43a693F3a957F9FeB5cC08AFA031E37a0"
+@pytest.mark.parametrize("mnemonic,expected_addresses", [
+    (
+        # Metamask
+        # https://github.com/MetaMask/eth-hd-keyring/blob/master/test/index.js
+        "finish oppose decorate face calm tragic certain desk hour urge dinosaur mango",
+        [
+            "0x1c96099350f13D558464eC79B9bE4445AA0eF579",
+            "0x1b00AeD43a693F3a957F9FeB5cC08AFA031E37a0",
+        ],
+    ),
+    (
+        # Ganache
+        # https://github.com/trufflesuite/ganache-core/blob/master/test/accounts.js
+        "into trim cross then helmet popular suit hammer cart shrug oval student",
+        [
+            "0x604a95C9165Bc95aE016a5299dd7d400dDDBEa9A",
+        ],
+    ),
+])
+def test_account_derivation(mnemonic, expected_addresses):
+    for idx, addr in enumerate(expected_addresses):
+        a = Account.from_mnemonic(mnemonic, account_index=idx)
+        assert a.address == addr
 
 
 def test_account_restore():
