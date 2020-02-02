67cff1239e3ecc58d1c41d75bf11a5a7df9e6a7e
Bryant Eisenbach <fubuloubu@gmail.com>
2020-02-01 22:57:59 -0500

    refactor: Added base path as an available constant

--- eth_account/hdaccount/__init__.py
+++ eth_account/hdaccount/__init__.py
@@ -5,9 +5,11 @@ from .mnemonic import (
     Mnemonic,
 )
 
+ETHEREUM_BASE_PATH = "m/44'/60'/0'/0"
+
 
 def derive_ethereum_key(seed: bytes, account_index: int=0):
-    return HDPath(f"m/44'/60'/0'/0/{account_index}").derive(seed)
+    return HDPath(f"{ETHEREUM_BASE_PATH}/{account_index}").derive(seed)
 
 
 def seed_from_mnemonic(words: str, passphrase="") -> bytes:
