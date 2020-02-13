cb228d54cad5f075f51634d1483234758f4ef68d
Bryant Eisenbach <fubuloubu@gmail.com>
2020-02-13 13:46:19 -0500

    refactor: Remove mutation

--- eth_account/hdaccount/mnemonic.py
+++ eth_account/hdaccount/mnemonic.py
@@ -26,17 +26,41 @@ import hashlib
 import os
 import unicodedata
 
+from eth_utils import (
+    ValidationError,
+    combomethod,
+)
+
 PBKDF2_ROUNDS = 2048
+VALID_SEED_SIZES = [16, 20, 24, 28, 32]
+VALID_WORD_LENGTHS = [12, 15, 18, 21, 24]
+WORDLIST_DIR = os.path.join(os.path.dirname(__file__), "wordlist")
 
 
 class ConfigurationError(Exception):
     pass
 
 
+def normalize_string(txt):
+    if isinstance(txt, bytes):
+        utxt = txt.decode("utf8")
+    elif isinstance(txt, str):
+        utxt = txt
+    else:
+        raise TypeError("String value expected")
+
+    return unicodedata.normalize("NFKD", utxt)
+
+
 class Mnemonic(object):
     def __init__(self, language):
+        if language not in self.list_languages():
+            raise ValidationError(
+                f'Invalid language choice "{language}", must be one of {self.list_langauges()}'
+            )
+        self.language = language
         self.radix = 2048
-        with open(f"{self._get_directory()}/{language}.txt", "r", encoding="utf-8") as f:
+        with open(f"{WORDLIST_DIR}/{language}.txt", "r", encoding="utf-8") as f:
             self.wordlist = [w.strip() for w in f.readlines()]
         if len(self.wordlist) != self.radix:
             raise ConfigurationError(
@@ -44,88 +68,71 @@ class Mnemonic(object):
                 f"but it contains {len(self.wordlist)} words."
             )
 
-    @classmethod
-    def _get_directory(cls):
-        return os.path.join(os.path.dirname(__file__), "wordlist")
-
-    @classmethod
-    def list_languages(cls):
+    @combomethod
+    def list_languages(self):
         return [
             f.split(".")[0]
-            for f in os.listdir(cls._get_directory())
+            for f in os.listdir(WORDLIST_DIR)
             if f.endswith(".txt")
         ]
 
     @classmethod
-    def normalize_string(cls, txt):
-        if isinstance(txt, bytes):
-            utxt = txt.decode("utf8")
-        elif isinstance(txt, str):
-            utxt = txt
-        else:
-            raise TypeError("String value expected")
-
-        return unicodedata.normalize("NFKD", utxt)
-
-    @classmethod
-    def detect_language(cls, code):
-        code = cls.normalize_string(code)
-        first = code.split(" ")[0]
+    def detect_language(cls, raw_mnemonic):
+        mnemonic = normalize_string(raw_mnemonic)
+        first = mnemonic.split(" ")[0]
         languages = cls.list_languages()
 
         for lang in languages:
-            mnemo = cls(lang)
-            if first in mnemo.wordlist:
+            if first in cls(lang).wordlist:
                 return lang
 
         raise ConfigurationError("Language not detected")
 
-    def generate(self, strength=128):
-        if strength not in [128, 160, 192, 224, 256]:
+    def generate(self, num_words=12):
+        if num_words not in VALID_WORD_LENGTHS:
             raise ValueError(
-                "Strength should be one of the following: [128, 160, 192, 224, 256]"
+                f"Invalid choice for number of words: {num_words}, should be one of "
+                f"{VALID_WORD_LENGTHS}"
             )
-        return self.to_mnemonic(os.urandom(strength // 8))
+        return self.to_mnemonic(os.urandom(4 * num_words // 3))  # 4/3 bytes per word
 
-    def to_mnemonic(self, data):
-        if len(data) not in [16, 20, 24, 28, 32]:
+    def to_mnemonic(self, seed):
+        if len(seed) not in VALID_SEED_SIZES:
             raise ValueError(
-                "Data length should be one of the following: [16, 20, 24, 28, 32]"
-                % len(data)
+                f"Invalid data length {len(seed)}, should be one of "
+                f"{VALID_WORD_LENGTHS}"
             )
-        h = hashlib.sha256(data).hexdigest()
-        b = (
-            bin(int(binascii.hexlify(data), 16))[2:].zfill(len(data) * 8) +
-            bin(int(h, 16))[2:].zfill(256)[: len(data) * 8 // 32]
+        checksum = hashlib.sha256(seed).hexdigest()
+        bits = (
+            bin(int(binascii.hexlify(seed), 16))[2:].zfill(len(seed) * 8) +
+            bin(int(checksum, 16))[2:].zfill(256)[: len(seed) * 8 // 32]
         )
         result = []
-        for i in range(len(b) // 11):
-            idx = int(b[i * 11: (i + 1) * 11], 2)
+        for i in range(len(bits) // 11):
+            idx = int(bits[i * 11: (i + 1) * 11], 2)
             result.append(self.wordlist[idx])
-        if (
-            self.detect_language(" ".join(result)) == "japanese"
-        ):  # Japanese must be joined by ideographic space.
+        if self.language == "japanese":  # Japanese must be joined by ideographic space.
             result_phrase = u"\u3000".join(result)
         else:
             result_phrase = " ".join(result)
         return result_phrase
 
     def check(self, mnemonic):
-        mnemonic = self.normalize_string(mnemonic).split(" ")
+        mnemonic = normalize_string(mnemonic).split(" ")
         # list of valid mnemonic lengths
-        if len(mnemonic) not in [12, 15, 18, 21, 24]:
+        if len(mnemonic) not in VALID_WORD_LENGTHS:
             return False
         try:
             idx = map(lambda x: bin(self.wordlist.index(x))[2:].zfill(11), mnemonic)
-            b = "".join(idx)
+            encoded_seed = "".join(idx)
         except ValueError:
             return False
-        l = len(b)  # noqa: E741
-        d = b[: l // 33 * 32]
-        h = b[-l // 33:]
-        nd = binascii.unhexlify(hex(int(d, 2))[2:].rstrip("L").zfill(l // 33 * 8))
-        nh = bin(int(hashlib.sha256(nd).hexdigest(), 16))[2:].zfill(256)[: l // 33]
-        return h == nh
+        l = len(encoded_seed)  # noqa: E741
+        bits = encoded_seed[: l // 33 * 32]
+        stored_checksum = encoded_seed[-l // 33:]
+        raw_seed = binascii.unhexlify(hex(int(bits, 2))[2:].rstrip("L").zfill(l // 33 * 8))
+        checksum = bin(int(hashlib.sha256(raw_seed).hexdigest(), 16))[2:].zfill(256)[: l // 33]
+        return stored_checksum == checksum
 
     def expand_word(self, prefix):
         if prefix in self.wordlist:
@@ -143,17 +150,24 @@ class Mnemonic(object):
         return " ".join(map(self.expand_word, mnemonic.split(" ")))
 
     @classmethod
-    def to_seed(cls, mnemonic, passphrase=""):
-        mnemonic = cls.normalize_string(mnemonic)
-        passphrase = cls.normalize_string(passphrase)
+    def to_seed(cls, checked_mnemonic: str, passphrase: str="") -> bytes:
+        """
+        :param str raw_checked_mnemonic: Must be a correct, fully-expended BIP39 seed phrase.
+        :param str raw_passphrase: Encryption passphrase used to secure the mnemonic.
+        :returns bytes: 64 bytes of raw seed material from PRNG
+        """
+        mnemonic = normalize_string(checked_mnemonic)
         # NOTE: This domain separater ("mnemonic") is added per BIP39 spec to the passphrase
         # https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki#from-mnemonic-to-seed
-        passphrase = "mnemonic" + passphrase
-        mnemonic = mnemonic.encode("utf-8")
-        passphrase = passphrase.encode("utf-8")
+        salt = "mnemonic" + normalize_string(passphrase)
         # From BIP39:
         #   To create a binary seed from the mnemonic, we use the PBKDF2 function with a
         # mnemonic sentence (in UTF-8 NFKD) used as the password and the string "mnemonic"
         # and passphrase (again in UTF-8 NFKD) used as the salt.
-        stretched = hashlib.pbkdf2_hmac("sha512", mnemonic, passphrase, PBKDF2_ROUNDS)
+        stretched = hashlib.pbkdf2_hmac(
+            "sha512",
+            mnemonic.encode("utf-8"),
+            salt.encode("utf-8"),
+            PBKDF2_ROUNDS,
+        )
         return stretched[:64]
