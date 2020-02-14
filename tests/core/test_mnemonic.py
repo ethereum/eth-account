03a5f9113666bf121fa624ecb8646920ace805b2
Bryant Eisenbach <fubuloubu@gmail.com>
2020-02-14 02:11:29 -0500

    feat: Added support for all official BIP39 wordlists

--- tests/core/test_mnemonic.py
+++ tests/core/test_mnemonic.py
@@ -25,6 +25,7 @@ import pytest
 from eth_account.hdaccount.mnemonic import (
     ConfigurationError,
     Mnemonic,
+    normalize_string,
 )
 
 
@@ -58,6 +59,13 @@ def test_expand():
     m = Mnemonic("english")
     assert "access" == m.expand("access")
     assert "access access acb acc act action" == m.expand("access acce acb acc act acti")
+
+
+def test_languages():
+    for lang in Mnemonic.list_languages():
+        m = Mnemonic(lang)
+        mnemonic = m.generate()
+        assert m.check(mnemonic)
 
 
 @pytest.mark.parametrize("entropy,expected_mnemonic,expected_seed", [
@@ -261,4 +269,234 @@ def test_english_mnemonics(entropy, expected_mnemonic, expected_seed):
     assert mnemonic == expected_mnemonic
 
     seed = Mnemonic.to_seed(mnemonic, passphrase="TREZOR")
+    assert seed.hex() == expected_seed
+
+
+@pytest.mark.parametrize("entropy,expected_mnemonic,passphrase,expected_seed", [
+    (
+        "00000000000000000000000000000000",
+        "あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　"
+        "あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　あおぞら",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "a262d6fb6122ecf45be09c50492b31f92e9beb7d9a845987a02cefda57a15f9c"
+        "467a17872029a9e92299b5cbdf306e3a0ee620245cbd508959b6cb7ca637bd55",
+    ),
+    (
+        "7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f",
+        "そつう　れきだい　ほんやく　わかす　りくつ　ばいか　"
+        "ろせん　やちん　そつう　れきだい　ほんやく　わかめ",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "aee025cbe6ca256862f889e48110a6a382365142f7d16f2b9545285b3af64e54"
+        "2143a577e9c144e101a6bdca18f8d97ec3366ebf5b088b1c1af9bc31346e60d9",
+    ),
+    (
+        "80808080808080808080808080808080",
+        "そとづら　あまど　おおう　あこがれる　いくぶん　けいけん　"
+        "あたえる　いよく　そとづら　あまど　おおう　あかちゃん",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "e51736736ebdf77eda23fa17e31475fa1d9509c78f1deb6b4aacfbd760a7e2ad"
+        "769c714352c95143b5c1241985bcb407df36d64e75dd5a2b78ca5d2ba82a3544",
+    ),
+    (
+        "ffffffffffffffffffffffffffffffff",
+        "われる　われる　われる　われる　われる　われる　"
+        "われる　われる　われる　われる　われる　ろんぶん",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "4cd2ef49b479af5e1efbbd1e0bdc117f6a29b1010211df4f78e2ed4008286579"
+        "3e57949236c43b9fe591ec70e5bb4298b8b71dc4b267bb96ed4ed282c8f7761c",
+    ),
+    (
+        "000000000000000000000000000000000000000000000000",
+        "あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　"
+        "あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　"
+        "あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　あらいぐま",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "d99e8f1ce2d4288d30b9c815ae981edd923c01aa4ffdc5dee1ab5fe0d4a3e139"
+        "66023324d119105aff266dac32e5cd11431eeca23bbd7202ff423f30d6776d69",
+    ),
+    (
+        "7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f",
+        "そつう　れきだい　ほんやく　わかす　りくつ　ばいか　"
+        "ろせん　やちん　そつう　れきだい　ほんやく　わかす　"
+        "りくつ　ばいか　ろせん　やちん　そつう　れいぎ",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "eaaf171efa5de4838c758a93d6c86d2677d4ccda4a064a7136344e975f91fe61"
+        "340ec8a615464b461d67baaf12b62ab5e742f944c7bd4ab6c341fbafba435716",
+    ),
+    (
+        "808080808080808080808080808080808080808080808080",
+        "そとづら　あまど　おおう　あこがれる　いくぶん　けいけん　"
+        "あたえる　いよく　そとづら　あまど　おおう　あこがれる　"
+        "いくぶん　けいけん　あたえる　いよく　そとづら　いきなり",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "aec0f8d3167a10683374c222e6e632f2940c0826587ea0a73ac5d0493b6a6325"
+        "90179a6538287641a9fc9df8e6f24e01bf1be548e1f74fd7407ccd72ecebe425",
+    ),
+    (
+        "ffffffffffffffffffffffffffffffffffffffffffffffff",
+        "われる　われる　われる　われる　われる　われる　"
+        "われる　われる　われる　われる　われる　われる　"
+        "われる　われる　われる　われる　われる　りんご",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "f0f738128a65b8d1854d68de50ed97ac1831fc3a978c569e415bbcb431a6a671"
+        "d4377e3b56abd518daa861676c4da75a19ccb41e00c37d086941e471a4374b95",
+    ),
+    (
+        "0000000000000000000000000000000000000000000000000000000000000000",
+        "あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　"
+        "あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　"
+        "あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　"
+        "あいこくしん　あいこくしん　あいこくしん　あいこくしん　あいこくしん　いってい",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "23f500eec4a563bf90cfda87b3e590b211b959985c555d17e88f46f7183590cd"
+        "5793458b094a4dccc8f05807ec7bd2d19ce269e20568936a751f6f1ec7c14ddd",
+    ),
+    (
+        "7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f",
+        "そつう　れきだい　ほんやく　わかす　りくつ　ばいか　"
+        "ろせん　やちん　そつう　れきだい　ほんやく　わかす　"
+        "りくつ　ばいか　ろせん　やちん　そつう　れきだい　"
+        "ほんやく　わかす　りくつ　ばいか　ろせん　まんきつ",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "cd354a40aa2e241e8f306b3b752781b70dfd1c69190e510bc1297a9c5738e833"
+        "bcdc179e81707d57263fb7564466f73d30bf979725ff783fb3eb4baa86560b05",
+    ),
+    (
+        "8080808080808080808080808080808080808080808080808080808080808080",
+        "そとづら　あまど　おおう　あこがれる　いくぶん　けいけん　"
+        "あたえる　いよく　そとづら　あまど　おおう　あこがれる　"
+        "いくぶん　けいけん　あたえる　いよく　そとづら　あまど　"
+        "おおう　あこがれる　いくぶん　けいけん　あたえる　うめる",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "6b7cd1b2cdfeeef8615077cadd6a0625f417f287652991c80206dbd82db17bf3"
+        "17d5c50a80bd9edd836b39daa1b6973359944c46d3fcc0129198dc7dc5cd0e68",
+    ),
+    (
+        "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+        "われる　われる　われる　われる　われる　われる　"
+        "われる　われる　われる　われる　われる　われる　"
+        "われる　われる　われる　われる　われる　われる　"
+        "われる　われる　われる　われる　われる　らいう",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "a44ba7054ac2f9226929d56505a51e13acdaa8a9097923ca07ea465c4c7e294c"
+        "038f3f4e7e4b373726ba0057191aced6e48ac8d183f3a11569c426f0de414623",
+    ),
+    (
+        "77c2b00716cec7213839159e404db50d",
+        "せまい　うちがわ　あずき　かろう　めずらしい　だんち　"
+        "ますく　おさめる　ていぼう　あたる　すあな　えしゃく",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "344cef9efc37d0cb36d89def03d09144dd51167923487eec42c487f742890854"
+        "6fa31a3c26b7391a2b3afe7db81b9f8c5007336b58e269ea0bd10749a87e0193",
+    ),
+    (
+        "b63a9c59a6e641f288ebc103017f1da9f8290b3da6bdef7b",
+        "ぬすむ　ふっかつ　うどん　こうりつ　しつじ　りょうり　"
+        "おたがい　せもたれ　あつめる　いちりゅう　はんしゃ　ごますり　"
+        "そんけい　たいちょう　らしんばん　ぶんせき　やすみ　ほいく",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "b14e7d35904cb8569af0d6a016cee7066335a21c1c67891b01b83033cadb3e8a"
+        "034a726e3909139ecd8b2eb9e9b05245684558f329b38480e262c1d6bc20ecc4",
+    ),
+    (
+        "3e141609b97933b66a060dcddc71fad1d91677db872031e85f4c015c5e7e8982",
+        "くのう　てぬぐい　そんかい　すろっと　ちきゅう　ほあん　"
+        "とさか　はくしゅ　ひびく　みえる　そざい　てんすう　"
+        "たんぴん　くしょう　すいようび　みけん　きさらぎ　げざん　"
+        "ふくざつ　あつかう　はやい　くろう　おやゆび　こすう",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "32e78dce2aff5db25aa7a4a32b493b5d10b4089923f3320c8b287a77e5124554"
+        "43298351beb3f7eb2390c4662a2e566eec5217e1a37467af43b46668d515e41b",
+    ),
+    (
+        "0460ef47585604c5660618db2e6a7e7f",
+        "あみもの　いきおい　ふいうち　にげる　ざんしょ　じかん　"
+        "ついか　はたん　ほあん　すんぽう　てちがい　わかめ",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "0acf902cd391e30f3f5cb0605d72a4c849342f62bd6a360298c7013d714d7e58"
+        "ddf9c7fdf141d0949f17a2c9c37ced1d8cb2edabab97c4199b142c829850154b",
+    ),
+    (
+        "72f60ebac5dd8add8d2a25a797102c3ce21bc029c200076f",
+        "すろっと　にくしみ　なやむ　たとえる　へいこう　すくう　"
+        "きない　けってい　とくべつ　ねっしん　いたみ　せんせい　"
+        "おくりがな　まかい　とくい　けあな　いきおい　そそぐ",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "9869e220bec09b6f0c0011f46e1f9032b269f096344028f5006a6e69ea5b0b8a"
+        "fabbb6944a23e11ebd021f182dd056d96e4e3657df241ca40babda532d364f73",
+    ),
+    (
+        "2c85efc7f24ee4573d2b81a6ec66cee209b2dcbd09d8eddc51e0215b0b68e416",
+        "かほご　きうい　ゆたか　みすえる　もらう　がっこう　"
+        "よそう　ずっと　ときどき　したうけ　にんか　はっこう　"
+        "つみき　すうじつ　よけい　くげん　もくてき　まわり　"
+        "せめる　げざい　にげる　にんたい　たんそく　ほそく",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "713b7e70c9fbc18c831bfd1f03302422822c3727a93a5efb9659bec6ad8d6f2c"
+        "1b5c8ed8b0b77775feaf606e9d1cc0a84ac416a85514ad59f5541ff5e0382481",
+    ),
+    (
+        "eaebabb2383351fd31d703840b32e9e2",
+        "めいえん　さのう　めだつ　すてる　きぬごし　ろんぱ　"
+        "はんこ　まける　たいおう　さかいし　ねんいり　はぶらし",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "06e1d5289a97bcc95cb4a6360719131a786aba057d8efd603a547bd254261c2a"
+        "97fcd3e8a4e766d5416437e956b388336d36c7ad2dba4ee6796f0249b10ee961",
+    ),
+    (
+        "7ac45cfe7722ee6c7ba84fbc2d5bd61b45cb2fe5eb65aa78",
+        "せんぱい　おしえる　ぐんかん　もらう　きあい　きぼう　"
+        "やおや　いせえび　のいず　じゅしん　よゆう　きみつ　"
+        "さといも　ちんもく　ちわわ　しんせいじ　とめる　はちみつ",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "1fef28785d08cbf41d7a20a3a6891043395779ed74503a5652760ee8c24dfe60"
+        "972105ee71d5168071a35ab7b5bd2f8831f75488078a90f0926c8e9171b2bc4a",
+    ),
+    (
+        "4fa1a8bc3e6d80ee1316050e862c1812031493212b7ec3f3bb1b08f168cabeef",
+        "こころ　いどう　きあつ　そうがんきょう　へいあん　せつりつ　"
+        "ごうせい　はいち　いびき　きこく　あんい　おちつく　"
+        "きこえる　けんとう　たいこ　すすめる　はっけん　ていど　"
+        "はんおん　いんさつ　うなぎ　しねま　れいぼう　みつかる",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "43de99b502e152d4c198542624511db3007c8f8f126a30818e856b2d8a20400d"
+        "29e7a7e3fdd21f909e23be5e3c8d9aee3a739b0b65041ff0b8637276703f65c2",
+    ),
+    (
+        "18ab19a9f54a9274f03e5209a2ac8a91",
+        "うりきれ　さいせい　じゆう　むろん　とどける　ぐうたら　"
+        "はいれつ　ひけつ　いずれ　うちあわせ　おさめる　おたく",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "3d711f075ee44d8b535bb4561ad76d7d5350ea0b1f5d2eac054e869ff7963cdc"
+        "e9581097a477d697a2a9433a0c6884bea10a2193647677977c9820dd0921cbde",
+    ),
+    (
+        "18a2e1d81b8ecfb2a333adcb0c17a5b9eb76cc5d05db91a4",
+        "うりきれ　うねる　せっさたくま　きもち　めんきょ　へいたく　"
+        "たまご　ぜっく　びじゅつかん　さんそ　むせる　せいじ　"
+        "ねくたい　しはらい　せおう　ねんど　たんまつ　がいけん",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "753ec9e333e616e9471482b4b70a18d413241f1e335c65cd7996f32b66cf9554"
+        "6612c51dcf12ead6f805f9ee3d965846b894ae99b24204954be80810d292fcdd",
+    ),
+    (
+        "15da872c95a13dd738fbf50e427583ad61f18fd99f628c417a61cf8343c90419",
+        "うちゅう　ふそく　ひしょ　がちょう　うけもつ　めいそう　"
+        "みかん　そざい　いばる　うけとる　さんま　さこつ　"
+        "おうさま　ぱんつ　しひょう　めした　たはつ　いちぶ　"
+        "つうじょう　てさぎょう　きつね　みすえる　いりぐち　かめれおん",
+        "㍍ガバヴァぱばぐゞちぢ十人十色",
+        "346b7321d8c04f6f37b49fdf062a2fddc8e1bf8f1d33171b65074531ec546d1d"
+        "3469974beccb1a09263440fc92e1042580a557fdce314e27ee4eabb25fa5e5fe",
+    ),
+])
+def test_japanese_mnemonics(entropy, expected_mnemonic, passphrase, expected_seed):
+    m = Mnemonic("japanese")
+    mnemonic = m.to_mnemonic(bytes.fromhex(entropy))
+    assert m.check(mnemonic)
+    # NOTE For some reason, the strings weren't appearing in normalized form as
+    #      copied rrom BIP39 test vectors
+    assert normalize_string(mnemonic) == normalize_string(expected_mnemonic)
+
+    seed = Mnemonic.to_seed(mnemonic, passphrase)
     assert seed.hex() == expected_seed
