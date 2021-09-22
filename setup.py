bd3dc2c0e85934b9c47980053d9f1d16a7540990
Jason Carver <ut96caarrs@snkmail.com>
2021-09-22 15:15:00 -0700

    Bump version: 0.5.5 → 0.5.6

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ with open('./README.md') as readme:
 setup(
     name='eth-account',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.5.5',
+    version='0.5.6',
     description="""eth-account: Sign Ethereum transactions and messages with local private keys""",
     long_description=long_description,
     long_description_content_type='text/markdown',
