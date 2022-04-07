5c3464357efc282e0b1cc9a08a3dbc21ac75ab0c
kclowes <kclowes@users.noreply.github.com>
2022-04-07 16:15:55 -0600

    Pin towncrier to version that doesn't duplicate version headings

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extras_require = {
         "Sphinx>=1.6.5,<5",
         "jinja2>=3.0.0,<3.1.0",  # jinja2<3.0 or >=3.1.0 cause doc build failures.
         "sphinx_rtd_theme>=0.1.9,<1",
-        "towncrier==19.2.0",  # towncrier doesn't follow semver
+        "towncrier==18.5.0",  # towncrier doesn't follow semver
     ],
     'dev': [
         "bumpversion>=0.5.3,<1",
