7f42d861709eb49e367466d5ebb400cce64bae9c
kclowes <kclowes@users.noreply.github.com>
2024-04-05 11:31:47 -0600

    move _parsePrivateKey to _parse_private_key (#267)

--- eth_account/account.py
+++ eth_account/account.py
@@ -262,7 +262,7 @@ class Account:
             # encrypt(). They correspond to the same-named methods in Account.*
             # but without the private key argument
         """
-        key = self._parsePrivateKey(private_key)
+        key = self._parse_private_key(private_key)
         return LocalAccount(key, self)
 
     @combomethod
@@ -335,7 +335,7 @@ class Account:
             )
         seed = seed_from_mnemonic(mnemonic, passphrase)
         private_key = key_from_seed(seed, account_path)
-        key = self._parsePrivateKey(private_key)
+        key = self._parse_private_key(private_key)
         return LocalAccount(key, self)
 
     @combomethod
@@ -611,7 +611,7 @@ class Account:
         if len(msg_hash_bytes) != 32:
             raise ValueError("The message hash must be exactly 32-bytes")
 
-        key = self._parsePrivateKey(private_key)
+        key = self._parse_private_key(private_key)
 
         (v, r, s, eth_signature_bytes) = sign_message_hash(key, msg_hash_bytes)
         return SignedMessage(
@@ -794,7 +794,7 @@ class Account:
         )
 
     @combomethod
-    def _parsePrivateKey(self, key):
+    def _parse_private_key(self, key):
         """
         Generate a :class:`eth_keys.datatypes.PrivateKey` from the provided key.
 
