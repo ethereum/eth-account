34ec1befc7503731dc9e903d5e06b6fc806c82a4
kclowes <kclowes@users.noreply.github.com>
2024-04-08 15:13:58 -0600

    Open up Pydantic requirement (#271)

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         "hexbytes>=1.2.0",
         "rlp>=1.0.0",
         "ckzg>=0.4.3",
-        "pydantic>=2.4.0",
+        "pydantic>=2.0.0",
     ],
     python_requires=">=3.8, <4",
     extras_require=extras_require,
