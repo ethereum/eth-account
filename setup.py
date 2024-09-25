8c9e29ee4ce9e602fe8a0affbcd97e0befb3e651
kclowes <kclowes@users.noreply.github.com>
2024-09-25 14:26:38 -0600

    Bump version: 0.13.3 → 0.13.4

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ with open("./README.md") as readme:
 setup(
     name="eth-account",
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version="0.13.3",
+    version="0.13.4",
     description="""eth-account: Sign Ethereum transactions and messages with local private keys""",
     long_description=long_description,
     long_description_content_type="text/markdown",
